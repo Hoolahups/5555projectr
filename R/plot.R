841602c862e68607c1c51b43e03be7591bf8880f
matthewwhite1 <mrmatthewwins@gmail.com>
2024-04-21 16:01:00 -0600

    kind of made barplot

--- R/plot.R
+++ R/plot.R
@@ -1,3 +1,22 @@
 stars.player <- function(player, vars, ...) {
-  stars(...)
+  stars(player)
+}
+
+barplot.team <- function(team, stat, players, ...) {
+  player_name <- vector()
+  stat_vec <- vector()
+  for (i in seq_along(team)) {
+    player_name <- c(player_name, as.character(team[[i]]["Player"]))
+    stat_vec <- c(stat_vec, as.numeric(team[[i]][stat]))
+  }
+  bar_data <- data.frame(player_name, stat_vec)
+  barplot(bar_data$stat_vec, names.arg = bar_data$player_name, ...)
+}
+
+
+# example team class to mess around with
+team <- list()
+for (i in 1:13) {
+  player <- as.list(bask[i, ])
+  team[[i]] <- player
 }
