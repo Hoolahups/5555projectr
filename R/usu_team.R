e1d9a9331d423451fd1d5d76388b3deffc09d374
nbrimhall <nbrimhall.01@gmail.com>
2024-04-22 15:54:49 -0600

    Lots of issues need to be fixed...

---
+++ R/usu_team.R
@@ -0,0 +1,12 @@
+#' USU 2023-2024 Men's Team
+#'
+#' Player statistics from the
+#'
+#' @format ## `student_life`
+#' A data frame?? list of lists??
+#' \describe{
+#'   \item{Number}{Player jersey number}
+#'   ...
+#' }
+#' @source <https://utahstateaggies.com/sports/mens-basketball/stats/2023-24>
+"usu_2023"
