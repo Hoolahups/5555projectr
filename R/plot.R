f1838f76af5969574e31688fee8e880eaf4ae20d
matthewwhite1 <mrmatthewwins@gmail.com>
2024-04-23 16:00:32 -0600

    basic star plot is actually working! basic vignette can probably be made

--- R/plot.R
+++ R/plot.R
@@ -6,48 +6,41 @@
 bask <- get_data("https://utahstateaggies.com/sports/mens-basketball/stats")
 
 # Starplot function: Uhhhhhhh
-# player_star <- function(team, player, vars, ...) {
-#   # Prepare data for plotting
-#   # 'FG%', '3PT%', 'FT%', scoring_avg, avg_re, AST, STL, BLK
-#   team[[player]]$AST <- as.numeric(team[[player]]$AST) / as.numeric(team[[player]]$GP)
-#   team[[player]]$STL <- as.numeric(team[[player]]$STL) / as.numeric(team[[player]]$GP)
-#   team[[player]]$BLK <- as.numeric(team[[player]]$BLK) / as.numeric(team[[player]]$GP)
-#   for (i in 2:length(team$player)) {
-#     team[[player]][i] <- as.numeric(team[[player]][i]) / as.numeric(team$avg[i])
-#   }
-#   stat_min <- rep(0, 8)
-#   stat_max <- rep(max(as.numeric(team[[player]][2:length(team[[player]])])), 8)
-#   player_row <- as.numeric(team[[player]][c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
-#                          "avg_re", "AST", "STL", "BLK")])
-#   star_data <- data.frame(rbind(stat_max, stat_min, player_row))
-#   names(star_data) <- c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
-#                         "avg_re", "AST", "STL", "BLK")
-#
-#   # Create radar/star plot using fmsb package
-#   # https://r-graph-gallery.com/142-basic-radar-chart.html
-#   fmsb::radarchart(star_data)
-# }
+player_star <- function(team, player, vars, ...) {
+  # Default vars: 'FG%', '3PT%', 'FT%', scoring_avg, avg_re, AST, STL, BLK
 
-# Test code for starplot
-# bask$osobor_great$AST <- as.numeric(bask$osobor_great$AST) / as.numeric(bask$osobor_great$GP)
-# bask$osobor_great$STL <- as.numeric(bask$osobor_great$STL) / as.numeric(bask$osobor_great$GP)
-# bask$osobor_great$BLK <- as.numeric(bask$osobor_great$BLK) / as.numeric(bask$osobor_great$GP)
-# for (i in 2:length(bask$osobor_great)) {
-#   bask$osobor_great[i] <- as.numeric(bask$osobor_great[i]) / as.numeric(bask$avg[i])
-# }
-# stat_min <- rep(0, 8)
-# stat_max <- rep(max(as.numeric(bask$osobor_great[2:length(bask$osobor_great)])), 8)
-# player_row <- as.numeric(bask$osobor_great[c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
-#                                              "avg_re", "AST", "STL", "BLK")])
-# star_data <- data.frame(rbind(stat_max, stat_min, player_row))
-# names(star_data) <- c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
-#                       "avg_re", "AST", "STL", "BLK")
-# fmsb::radarchart(star_data)
+  # Dividing by games played caused numbers to be 0 later on,
+  # so commenting out for now
+  # team[[player]]$AST <- as.numeric(team[[player]]$AST) / as.numeric(team[[player]]$GP)
+  # team[[player]]$STL <- as.numeric(team[[player]]$STL) / as.numeric(team[[player]]$GP)
+  # team[[player]]$BLK <- as.numeric(team[[player]]$BLK) / as.numeric(team[[player]]$GP)
+
+  # Divide each value by average team value
+  for (i in 2:length(team[[player]])) {
+    team[[player]][i] <- as.numeric(team[[player]][i]) / as.numeric(team$avg[i])
+  }
+
+  # Create data frame for radarchart, using first row as max values, second row
+  # as min values, and third row as actual values
+  stat_min <- rep(0, 8)
+  stat_max <- rep(max(as.numeric(team[[player]][2:length(team[[player]])])), 8)
+  player_row <- as.numeric(team[[player]][c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
+                         "avg_re", "AST", "STL", "BLK")])
+  star_data <- data.frame(rbind(stat_max, stat_min, player_row))
+  names(star_data) <- c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
+                        "avg_re", "AST", "STL", "BLK")
+
+  # Create radar/star plot using fmsb package
+  # https://r-graph-gallery.com/142-basic-radar-chart.html
+  fmsb::radarchart(star_data, pfcol = grDevices::rgb(0.2,0.5,0.5,0.5),
+                   pty = NA)
+}
 
 # Barplot function: Plot one statistic across all players (TODO: choose which
 # players to plot statistic across)
 team_bar <- function(team, stat, players, ...) {
-  # Prepare data for plotting
+  # Extract player names and stats into two vectors, then put them in a data
+  # frame and arrange by descending order
   player_name <- vector()
   stat_vec <- vector()
   for (i in seq_along(team)) {
@@ -67,12 +60,15 @@ team_bar <- function(team, stat, players, ...) {
 
 # Scatterplot function: Plot the relationship between two stats
 compare_stats <- function(team, stat1, stat2, ...) {
+  # Extract stats into two vectors
   stat1_vec <- vector()
   stat2_vec <- vector()
   for (i in seq_along(team)) {
     stat1_vec <- c(stat1_vec, as.numeric(team[[i]][stat1]))
     stat2_vec <- c(stat2_vec, as.numeric(team[[i]][stat2]))
   }
+
+  # Plot scatterplot of two variables
   plot(stat1_vec, stat2_vec,
        xlab = stat1, ylab = stat2,
        main = paste0("Relationship Between ", stat1, " and ", stat2))
