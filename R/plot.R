0f03bff241803f44b307481d7ab8f0f68631a5b1
matthewwhite1 <mrmatthewwins@gmail.com>
2024-04-22 19:38:16 -0600

    completed basic star function finally

--- R/plot.R
+++ R/plot.R
@@ -2,19 +2,43 @@
 # class is a list hehe here is stuff for a new push
 
 # Starplot function: Uhhhhhhh
-player_star <- function(player, vars, ...) {
+player_star <- function(team, player, vars, ...) {
   # Prepare data for plotting
   # 'FG%', '3PT%', 'FT%', scoring_avg, avg_re, AST, STL, BLK
+  team[[player]]$AST <- as.numeric(team[[player]]$AST) / as.numeric(team[[player]]$GP)
+  team[[player]]$STL <- as.numeric(team[[player]]$STL) / as.numeric(team[[player]]$GP)
+  team[[player]]$BLK <- as.numeric(team[[player]]$BLK) / as.numeric(team[[player]]$GP)
+  for (i in 2:length(team$player)) {
+    team[[player]][i] <- as.numeric(team[[player]][i]) / as.numeric(team$avg[i])
+  }
   stat_min <- rep(0, 8)
-  stat_max <- rep(max(as.numeric(player[3:length(player)])), 8)
-  player_row <- player[c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
-                         "avg_re", "AST", "STL", "BLK")]
-  star_data <- data.frame(stat_max, stat_min, player_row)
+  stat_max <- rep(max(as.numeric(team[[player]][2:length(team[[player]])])), 8)
+  player_row <- as.numeric(team[[player]][c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
+                         "avg_re", "AST", "STL", "BLK")])
+  star_data <- data.frame(rbind(stat_max, stat_min, player_row))
+  names(star_data) <- c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
+                        "avg_re", "AST", "STL", "BLK")
 
   # Create radar/star plot using fmsb package
   # https://r-graph-gallery.com/142-basic-radar-chart.html
   fmsb::radarchart(star_data)
 }
+
+
+bask$osobor_great$AST <- as.numeric(bask$osobor_great$AST) / as.numeric(bask$osobor_great$GP)
+bask$osobor_great$STL <- as.numeric(bask$osobor_great$STL) / as.numeric(bask$osobor_great$GP)
+bask$osobor_great$BLK <- as.numeric(bask$osobor_great$BLK) / as.numeric(bask$osobor_great$GP)
+for (i in 2:length(bask$osobor_great)) {
+  bask$osobor_great[i] <- as.numeric(bask$osobor_great[i]) / as.numeric(bask$avg[i])
+}
+stat_min <- rep(0, 8)
+stat_max <- rep(max(as.numeric(bask$osobor_great[2:length(bask$osobor_great)])), 8)
+player_row <- as.numeric(bask$osobor_great[c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
+                                             "avg_re", "AST", "STL", "BLK")])
+star_data <- data.frame(rbind(stat_max, stat_min, player_row))
+names(star_data) <- c("FG_pct", "X3PT_pct", "FT_pct", "scoring_avg",
+                      "avg_re", "AST", "STL", "BLK")
+fmsb::radarchart(star_data)
 
 # Barplot function: Plot one statistic across all players (TODO: choose which
 # players to plot statistic across)
@@ -52,3 +76,4 @@ compare_stats <- function(team, stat1, stat2, ...) {
 
 # example team class to mess around with
 bask <- get_data("https://utahstateaggies.com/sports/mens-basketball/stats")
+
