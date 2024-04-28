9c9706815c88971898a2fca0f928b2a97e9048ac
matthewwhite1 <mrmatthewwins@gmail.com>
2024-04-28 13:44:54 -0600

    created basic team_dot() - changed team_bar() to team_dot() as Bean requested

--- R/plot.R
+++ R/plot.R
@@ -60,12 +60,12 @@ player_star <- function(team, player, vars = c("FG_pct", "X3PT_pct", "FT_pct",
                    pty = NA, title = player)
 }
 
-# Barplot function: Plot one statistic across all players (TODO: choose which
+# Dot plot function: Plot one statistic across all players (TODO: choose which
 # players to plot statistic across)
 
-#' Create a bar plots for players
+#' Create a dot plot for players
 #'
-#' This function generates a bar plot showing the values of a given statistic
+#' This function generates a dot plot showing the values of a given statistic
 #' across all players. Players can be selected also be specified.
 #'
 #' @param team A list representing the team data.
@@ -73,13 +73,13 @@ player_star <- function(team, player, vars = c("FG_pct", "X3PT_pct", "FT_pct",
 #' @param players A character vector specifying which players to include in the plot (coming soon).
 #' @param ... Additional arguments passed to other methods.
 #'
-#' @return A ggplot object representing the bar plot of the statistic across selected players.
+#' @return A baseR dotchart plot.
 #'
 #' @examples
-#' team_bar(usu_2023, 'FG_pct')
+#' team_dot(usu_2023, 'FG_pct')
 #'
 #' @export
-team_bar <- function(team, stat, players, ...) {
+team_dot <- function(team, stat, players, ...) {
   # Extract player names and stats into two vectors, then put them in a data
   # frame and arrange by descending order
   player_name <- vector()
@@ -88,16 +88,20 @@ team_bar <- function(team, stat, players, ...) {
     player_name <- c(player_name, names(team)[i])
     stat_vec <- c(stat_vec, as.numeric(team[[i]][stat]))
   }
-  bar_data <- data.frame(player_name, stat_vec) |>
-    dplyr::arrange(stat_vec)
+  dot_data <- data.frame(stat_vec, player_name)
+  row.names(dot_data) <- player_name
+  dot_data <- dot_data[order(dot_data$stat_vec), ]
+
+  graphics::dotchart(dot_data$stat_vec,
+                     labels = row.names(dot_data))
 
   # Create horizontal bar plot
-  ggplot2::ggplot(bar_data, ggplot2::aes(stat_vec, stats::reorder(player_name,
-                                                                  stat_vec))) +
-    ggplot2::geom_col() +
-    ggplot2::xlab(stat) +
-    ggplot2::ylab("Player") +
-    ggplot2::ggtitle(paste0(stat, " Between All Players"))
+  # ggplot2::ggplot(bar_data, ggplot2::aes(stat_vec, stats::reorder(player_name,
+  #                                                                 stat_vec))) +
+  #   ggplot2::geom_col() +
+  #   ggplot2::xlab(stat) +
+  #   ggplot2::ylab("Player") +
+  #   ggplot2::ggtitle(paste0(stat, " Between All Players"))
 }
 
 # Scatterplot function: Plot the relationship between two stats
